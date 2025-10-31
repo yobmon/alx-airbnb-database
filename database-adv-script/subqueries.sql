ad14e2faebcda5e6d0b8f1e062a9dff3b0e42c8a
Yob <eyobsolomon33@gmail.com>
2025-10-31 09:26:17 +0300

    correlated sub query

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,10 +1,15 @@
-SELECT name
+SELECT *
 FROM property
 WHERE property_id IN (
-    SELECT property_id  
-    FROM review  
-    WHERE rating > 4
+    SELECT property_id
+    FROM review
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
 );
-SELECT first_name 
-FROM "User"
- WHERE user_id IN (SELECT user_id FROM "booking");
+SELECT u.user_id, u.first_name, u.last_name
+FROM "User" u
+WHERE (
+    SELECT COUNT(*)
+    FROM booking b
+    WHERE b.user_id = u.user_id
+) > 3;
